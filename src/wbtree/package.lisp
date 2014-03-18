b0740fb1d186fc7671fe8e6e0344b30f7f6beafe
dirk.esser <dirk.esser@github.deterministic-arts.de>
2014-03-18 13:29:53 +0100

    Exported wbtree-{min|max}imum-node

--- src/wbtree/package.lisp
+++ src/wbtree/package.lisp
@@ -29,7 +29,7 @@
            "WBTREE-FIND" "WBTREE-DIFFERENCE" "WBTREE-UNION" "WBTREE-INTERSECTION" 
            "WBTREE-ITERATOR" "WBTREE-EQUAL" "DEFINE-WBTREE" "WBTREE-LOWER-BOUNDARY-NODE"
            "WBTREE-UPPER-BOUNDARY-NODE" "WBTREE-CHECK-INVARIANTS" "WBTREE-REBALANCE"
-           "WBTREE-FOLD")
+           "WBTREE-FOLD" "WBTREE-MINIMUM-NODE" "WBTREE-MAXIMUM-NODE")
   (:documentation "Generalized weight-balanced binary search trees. This
 package provides a variant of the weight-balanced binary trees implemented
 in package DARTS.LIB.PTREE. The variant exposed here can be used with arbitrary
